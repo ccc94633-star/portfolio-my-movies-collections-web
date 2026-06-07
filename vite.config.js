bb8fb349840f06da6821149c9aa3493636c8a1a3
ccc94633-star <ccc94633@gmail.com>
2026-06-07 23:12:17 +0800

    build上去

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/portfolio-my-movies-collections-web/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
